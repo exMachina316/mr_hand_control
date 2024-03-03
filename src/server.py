6ef54630ae83bb7b93bd7e37dcbe93b948e9876f
exMachina316 <pythonprog24@gmail.com>
2024-03-03 23:11:05 +0530

    addded button press flags

--- src/server.py
+++ src/server.py
@@ -3,11 +3,16 @@ import asyncio
 
 import pyautogui as pg
 
+r_down = False
+l_down = False
+
 async def handle_connection(websocket, path):
+    global r_down, l_down
     async for message in websocket:
         width, height = pg.size()
 
         message = message.split(",")
+        print(message)
 
         x,y = pg.position()
 
@@ -20,14 +25,22 @@ async def handle_connection(websocket, path):
             pg.moveTo(x, y, duration=0.001)
 
         if "r_touch" in message:
+            print("right_button_down")
+            r_down = True
             pg.mouseDown(button="right")
-        else:
+        elif r_down:
+            print("right_button_up")
             pg.mouseUp(button="right")
+            r_down = False
         
         if "l_touch" in message:
+            print("left_button_down")
+            l_down = True
             pg.mouseDown(button="left")
-        else:
+        elif l_down:
+            print("left_button_up")
             pg.mouseUp(button="left")
+            l_down = False
 
         await websocket.send("Message received")
 
